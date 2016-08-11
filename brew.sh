357dc69885d20c7ea29302341008d6093c10e8ea
Hagay Ben-Shushan <hagaybens@gmail.com>
2016-08-11 13:28:27 +0300

    Add the brew bash version to /etc/shells and add installation of gpg

--- brew.sh
+++ brew.sh
@@ -26,11 +26,13 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# Add `/usr/local/bin/bash` to `/etc/shells` before
+sudo echo "/usr/local/bin/bash" >> /etc/shells
 # running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
+brew install gpg
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
