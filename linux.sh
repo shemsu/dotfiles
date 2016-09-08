e4f81e1b2c95641386e24cfd9b473422721cd79c
Hagay Ben Shushan <hagaybens@gmail.com>
2016-09-08 18:16:11 +0300

    Add basic linux setup script

---
+++ linux.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+sudo apt-get update
+
+sudo apt-get install git
+sudo apt-get install tmux
+sudo apt-get install vim
+sudo apt-get install xpdf
+sudo apt-get install lynx
+sudo apt-get install moreutils
+sudo apt-get install pip
+sudo apt-get install pip3
+sudo apt-get install build-essential
+sudo apt-get install python-dev python3-dev
+sudo apt-get install cmake
